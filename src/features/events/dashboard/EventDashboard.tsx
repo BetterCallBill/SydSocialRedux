e40594a283fff6ff24e1848c7117dfc0ec9b7c49
Billy <billy.wang.aus@gmail.com>
2024-04-10 14:03:27 +1000

    add the create & update function to Firestore

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -1,9 +1,43 @@
 import { Grid } from 'semantic-ui-react';
 import EventList from './EventList';
 import { useAppSelector } from '../../../app/store/store';
+import { useEffect, useState } from 'react';
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import { db } from '../../../app/config/firebase';
+import { useDispatch } from 'react-redux';
+import { AppEvent } from '../../../app/types/event';
+import { setEvents } from '../eventSlice';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 export default function EventDashboard() {
     const { events } = useAppSelector(state => state.events);
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const q = query(collection(db, 'events'));
+
+        const unsubscribe = onSnapshot(q, {
+            next: querySnapshot => {
+                const evts: AppEvent[] = [];
+
+                querySnapshot.forEach(doc => {
+                    evts.push({ id: doc.id, ...doc.data() } as AppEvent);
+                });
+
+                dispatch(setEvents(evts));
+                setLoading(false);
+            },
+            error: error => {
+                console.log('Collection error:', error);
+                setLoading(false);
+            },
+        });
+
+        return () => unsubscribe();
+    }, [dispatch]);
+
+    if (loading) return <LoadingComponent />
     
     return (
         <Grid>
--- src/features/events/eventSlice.ts
+++ src/features/events/eventSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 import { AppEvent } from '../../app/types/event'
+import { Timestamp } from 'firebase/firestore'
 
 /*
 State
@@ -26,11 +27,21 @@ export const eventSlice = createSlice({
     name: 'events',
     initialState,
     reducers: {
+        setEvents: {
+            reducer: (state, action: PayloadAction<AppEvent[]>) => {
+                state.events = action.payload;
+            },
+            prepare: (events: any) => {
+                let eventArray: AppEvent[] = [];
+                Array.isArray(events) ? eventArray = events : eventArray.push(events)
+                const mapped = eventArray.map((e: any) => {
+                    return {...e, date: (e.date as Timestamp).toDate().toISOString()}
+                });
+                return {payload: mapped}
+            }
+        },
         createEvent: (state, action) => {
             state.events.push(action.payload);
-        },
-        updateEvent: (state, action) => {
-            state.events[state.events.findIndex(evt => evt.id === action.payload.id)] = action.payload;
         },
         deleteEvent: (state, action) => {
             state.events.splice(state.events.findIndex(evt => evt.id === action.payload), 1)
@@ -38,4 +49,4 @@ export const eventSlice = createSlice({
     }
 })
 
-export const { createEvent, updateEvent, deleteEvent } = eventSlice.actions;
+export const { createEvent, deleteEvent, setEvents } = eventSlice.actions;
