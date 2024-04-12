52deb3410a37c15e3b649ca505ec2f9927ab2cf0
Billy <billy.wang.aus@gmail.com>
2024-04-13 00:33:29 +1000

    update profile functions

--- src/app/config/firebase.ts
+++ src/app/config/firebase.ts
@@ -3,6 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // Your web app's Firebase configuration
@@ -22,3 +23,4 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
