352ed4a50c2a75b4776ebf6188159fde3ad5e159
Billy <billy.wang.aus@gmail.com>
2024-04-19 09:49:56 +1000

    add pagination

--- src/features/profiles/ProfileHeader.tsx
+++ src/features/profiles/ProfileHeader.tsx
@@ -55,24 +55,26 @@ export default function ProfileHeader({ profile }: Props) {
                         <Statistic label="Followers" value={profile.followerCount || 0} />
                         <Statistic label="Following" value={profile.followingCount || 0} />
                     </Statistic.Group>
-                    <Divider />
 
                     {!isCurrentUser && (
-                        <Reveal animated="move">
-                            <Reveal.Content visible style={{ width: '100%' }}>
-                                <Button fluid color="teal" content={profile.isFollowing ? 'Following' : 'Not following'} />
-                            </Reveal.Content>
-                            <Reveal.Content hidden style={{ width: '100%' }}>
-                                <Button
-                                    basic
-                                    fluid
-                                    color={profile.isFollowing ? 'red' : 'green'}
-                                    content={profile.isFollowing ? 'Unfollow' : 'Follow'}
-                                    onClick={() => handleFollowToggle(!profile.isFollowing)}
-                                    loading={loading}
-                                />
-                            </Reveal.Content>
-                        </Reveal>
+                        <>
+                            <Divider />
+                            <Reveal animated="move">
+                                <Reveal.Content visible style={{ width: '100%' }}>
+                                    <Button fluid color="teal" content={profile.isFollowing ? 'Following' : 'Not following'} />
+                                </Reveal.Content>
+                                <Reveal.Content hidden style={{ width: '100%' }}>
+                                    <Button
+                                        basic
+                                        fluid
+                                        color={profile.isFollowing ? 'red' : 'green'}
+                                        content={profile.isFollowing ? 'Unfollow' : 'Follow'}
+                                        onClick={() => handleFollowToggle(!profile.isFollowing)}
+                                        loading={loading}
+                                    />
+                                </Reveal.Content>
+                            </Reveal>
+                        </>
                     )}
                 </Grid.Column>
             </Grid>
