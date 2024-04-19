e885445047aff0473bfde5985bb5fed015697b17
Billy <billy.wang.aus@gmail.com>
2024-04-19 11:10:06 +1000

    add unauth component & private routes

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -5,18 +5,27 @@ import EventDetail from '../../features/events/details/EventDetail';
 import EventForm from '../../features/events/form/EventForm';
 import AccountPage from '../../features/auth/AccoutPage';
 import ProfilePage from '../../features/profiles/ProfilePage';
+import RequireAuth from './RequireAuth';
+import UnauthComponent from '../layout/UnauthComponent';
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
         children: [
+            {
+                element: <RequireAuth />,
+                children: [
+                    // oops you need auth
+                    { path: '/manage/:id', element: <EventForm /> },
+                    { path: '/profiles/:id', element: <ProfilePage /> },
+                    { path: '/createEvent', element: <EventForm key="create" /> },
+                    { path: '/account', element: <AccountPage /> },
+                ],
+            },
             { path: '/events', element: <EventDashboard /> },
             { path: '/events/:id', element: <EventDetail /> },
-            { path: '/manage/:id', element: <EventForm /> },
-            { path: '/profiles/:id', element: <ProfilePage /> },
-            { path: '/createEvent', element: <EventForm key='create' /> },
-            { path: '/account', element: <AccountPage /> },
+            { path: '/unauthorised', element: <UnauthComponent /> },
         ],
     },
 ]);
