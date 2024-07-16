fd1dd362289f6253a96fa37683dca4cd12c03cb8
Billy <billy.wang.aus@gmail.com>
2024-07-16 17:22:24 +1000

    update CI/CD

--- src/app/config/firebase.ts
+++ src/app/config/firebase.ts
@@ -14,7 +14,6 @@ declare global {
 }
 
 // Your web app's Firebase configuration
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
